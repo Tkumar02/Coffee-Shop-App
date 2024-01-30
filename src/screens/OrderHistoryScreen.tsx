ea9093a9712dce0fba7d03efa5c036576743ea97
Tkumar02 <ta_265@yahoo.co.uk>
2024-01-30 23:17:46 +0000

    1-up to first bit of tab screen

---
+++ src/screens/OrderHistoryScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function OrderHistoryScreen() {
+  return (
+    <View>
+      <Text>OrderHistoryScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
